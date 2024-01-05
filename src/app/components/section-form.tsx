954ecf4340ea30b28d722476e008381209ec5589
lzag <luiz.sa.cruz@gmail.com>
2024-01-05 14:41:24 -0300

    send

--- src/app/components/section-form.tsx
+++ src/app/components/section-form.tsx
@@ -2,42 +2,52 @@
 import React,{useState} from 'react'
 import GridContainer from './grid';
 import { MdOutlineEmail } from "react-icons/md";
+import emailjs from '@emailjs/browser';
+
 
 function SectionForm() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        subject: '',
-        message: '',
-      });
-      const handleChange = (e: { target: { name: any; value: any; }; }) => {
-        const { name, value } = e.target;
-        setFormData({
-          ...formData,
-          [name]: value,
-        });
-      };
-    
-      const handleSubmit = async (e: any) => {
-        console.log('enviado')
-      };
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  function sendEmail(e: { preventDefault: () => void; }){
+    e.preventDefault();
+    if(name === '' || email === ''|| message==''){
+      alert("Preencha todos os dados")
+      return;
+    }
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email,
+    }
+    emailjs.send("service_iyqgili","template_1cbfqjj",templateParams,"Jz_f6I01jXwjFu9v0")
+    .then((response)=>{
+      console.log("EMAIL ENVIADO", response.status,response.text)
+      setName('')
+      setEmail('')
+      setMessage('')
+    },(err)=>{console.log("ERRO",err)})
+  }
+
+
     return ( 
     <section className='pt-10 bg-gray-200 p-4' id='form'>
-        <GridContainer className='flex justify-center items-center gap-6 max-md:grid-cols-1 bg-white rounded-xl shadow-lg'>
+        <GridContainer className='flex justify-center items-center gap-6 max-md:grid-cols-1 bg-white rounded-xl shadow-lg max-md:grid'>
         <div className='m-6'>
           <MdOutlineEmail size={48} color={'white'} className='rounded-full bg-secondary-500 py-2 px-2' />
           <h1 className='text-2xl font-bold'>Fale com a RC Consultoria</h1>
           <p className='text-black text-base'>Em breve nossos consultores entrarão em contato com você</p>
           </div>
-            <form onSubmit={handleSubmit} className=" bg-slate-50 shadow-md rounded-md w-[50%] m-6 py-4 px-4 ">
+            <form  onSubmit={sendEmail} className=" bg-slate-50 shadow-md rounded-md w-[50%] max-md:w-[80%]  m-6 py-4 px-4 ">
                     <div className="mb-6">
                         <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Nome</label>
                         <input
                         type="text"
                         id="name"
                         name="name"
-                        value={formData.name}
-                        onChange={handleChange}
+                        value={name}
+                        onChange={(e)=>setName(e.target.value)}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                         placeholder="Seu nome"
                         required
@@ -49,23 +59,10 @@ function SectionForm() {
                         type="email"
                         id="email"
                         name="email"
-                        value={formData.email}
-                        onChange={handleChange}
+                        value={email}
+                        onChange={(e)=>setEmail(e.target.value)}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                         placeholder="seuemail@exemplo.com"
-                        required
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900">Assunto</label>
-                        <input
-                        type="text"
-                        id="subject"
-                        name="subject"
-                        value={formData.subject}
-                        onChange={handleChange}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        placeholder="Assunto da mensagem"
                         required
                         />
                     </div>
@@ -74,15 +71,15 @@ function SectionForm() {
                         <textarea
                         id="message"
                         name="message"
-                        value={formData.message}
-                        onChange={handleChange}
+                        value={message}
+                        onChange={(e)=>setMessage(e.target.value)}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                         placeholder="Escreva sua mensagem"
                         rows={4}
                         required
                         ></textarea>
                     </div>
-                    <button type="submit" className="font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Enviar</button>
+                    <input type="submit" value="Enviar" className="font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center cursor-pointer"/>
              </form>
              {/* <div className='bg-white rounded-md px-4 py-4 text-center align-middle'>
                 <h1 className='font-bold text-4xl'>CONTATO</h1>
