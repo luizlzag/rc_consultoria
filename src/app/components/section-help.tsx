ead9d091d3d519401ad3b8bc36a2411a9a815bc2
lzag <luiz.sa.cruz@gmail.com>
2024-01-04 23:19:41 -0300

    responsividade

--- src/app/components/section-help.tsx
+++ src/app/components/section-help.tsx
@@ -6,16 +6,16 @@ import Image from 'next/image';
 function SectionHelp() {
     return ( 
     <section className='bg-secondary-400'>
-        <GridContainer className='flex justify-between items-center p-4  gap-6'>
-           <div className='flex gap-4'>
+        <GridContainer className='flex justify-between items-center max-md:p-1 p-4  gap-6'>
+           <div className='flex items-center gap-4'>
                 <Image 
                 src={consultor}
                 alt='consultor'
-                className='rounded-full w-20 '
+                className='rounded-full flex  w-20 max-md:h-16'
                 />
                 <div>
                     <h2 className='text-white text-2xl max-md:text-base'>PRECISA DE AJUDA?</h2>
-                    <h1 className='text-blue-950 text-4xl font-bold max-md:text-lg'>Fale com nossos especialistas</h1>
+                    <h1 className='text-blue-950 text-4xl font-bold max-md:text-base'>Fale com nossos especialistas</h1>
                 </div>
           </div>
             <div >
