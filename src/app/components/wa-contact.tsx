b5620c48f016fdea6880641357c2bd058fb281d1
lzag <luiz.sa.cruz@gmail.com>
2024-01-05 10:27:21 -0300

    ajuste visual

--- src/app/components/wa-contact.tsx
+++ src/app/components/wa-contact.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 function WaContact() {
     return ( 
-        <footer className='fixed bottom-3 right-5 cursor-pointer'>
+        <footer className='fixed bottom-3 right-5 cursor-pointer z-10'>
             <Image 
             src={wpp}
             alt='wpp'/>
